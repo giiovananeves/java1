7714e81ce0de6835b26bf146b9e2a3ac0b1bb0a8
giiovananeves <giovana.silva23@fatec.sp.gov.br>
2022-02-21 19:30:35 +0000

    atu

--- Mensagem2.java
+++ Mensagem2.java
@@ -1,5 +1,7 @@
 public class Mensagem2{
     public static void main(String[] ags){
         System.out.println("Fatec Pompeia");
+        int idade = 10;
+    System.out.println("Giovana tem " + (idade+10) + " anos " );
     }
 }
